3715f85a7aaca45141503a2467afea512bcaac3c
mfmasuyama <mfmasuyama@gmail.com>
2016-11-10 17:55:23 -0300

    Search artist.

--- src/app/index.js
+++ src/app/index.js
@@ -10,19 +10,24 @@ angular
 .controller('resultsCtrl', ['$scope', '$http', "ApiService", function($scope, $http, ApiService) {
     $scope.resultsCtrl = this;
 
-    ApiService.getArtists().then(function(artists) {
-        this.artists = artists;
-    }.bind(this));
+    this.getArtists = function(){
+        ApiService.getArtists($scope.resultsCtrl.artist).then(function(artists) {
+            this.artists = artists.items;
+            console.log(this.artists);
+        }.bind(this));
+    }
+
 }])
+
 .service('ApiService', ['$http', function($http) {
-    this.getArtists = function() {
+    this.getArtists = function(name) {
         return $http({
             method: 'GET',
-            url: "https://api.spotify.com/v1/search?q=" + this.artist + "&type=artist"
+            url: "https://api.spotify.com/v1/search?q=" + name + "&type=artist"
         }).then(function(response) {
             return response.data.artists;
-        }, function () {
-            console.error('La llamada fallo');
+        }, function() {
+            console.error('Error');
         })
     }
 }]);
