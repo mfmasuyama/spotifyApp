87290cdca1ecd5fe4f0214ec155194ea364068a8
mfmasuyama <mfmasuyama@gmail.com>
2016-11-14 16:08:55 -0300

    Updates.

--- src/app/routes.js
+++ src/app/routes.js
@@ -21,3 +21,9 @@ $routeProvider
         templateUrl: "app/views/results.html"
     })
 }
+
+/*
+$routeProvider.when('/view1/:param1/:param2', { templateUrl: 'partials/partial1.html', controller: 'MyCtrl1' });
+Then in your controller inject $routeParams:
+.controller('MyCtrl1', ['$scope','$routeParams', function($scope, $routeParams) { var param1 = $routeParams.param1; var param2 = $routeParams.param2; //change here from param1 to param2 ... }]);
+*/
