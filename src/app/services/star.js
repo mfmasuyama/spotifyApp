33b43910a46ff9b2af774a548fe8477a0ba803f6
mfmasuyama <mfmasuyama@gmail.com>
2016-11-18 17:52:48 -0300

    Updates on star.

--- src/app/services/star.js
+++ src/app/services/star.js
@@ -1,22 +1,36 @@
 export function StarService($localStorage) {
 
     this.click = function(item) { /* FIX */
-        if(typeof localStorage.getItem("favourites") === "null") {
-            this.favourites = new Array();
-        } else {
-            this.favourites = localStorage.getItem("favourites");
-        }
+        // if(typeof (Storage) !== "undefined") {
+        //     console.log(typeof (Storage));
+        //     if(typeof localStorage.getItem("favourites") !== "undefined" || typeof localStorage.getItem("favourites") === "null") {
+        //         console.log(typeof localStorage.getItem("favourites"));
+        //         let retrievedFavourites = localStorage.getItem("favourites");
+        //         this.favouritesAux = JSON.parse(retrievedFavourites);
+        //         console.log(typeof this.favouritesAux);
+        //         if(typeof this.favouritesAux === "null") {
+        //             this.favourites = new Array();
+        //         } else {
+        //             this.favourites = this.favouritesAux;
+        //             console.log(this.favourites);
+        //         }
+        //     }
+        // }
+
+        this.favourites = new Array();
+        //Favourites.
         let isFavourite = false;
-        for(let i=0; i<this.favourites.length; i++) {
+        for(let i=0; i<this.favourites.length; i++) { //Check if it's already a favourite.
             if(this.favourites[i].id === item.id) {
                 isFavourite = true;
                 this.favourites.splice(i,1);
             }
         }
-        if(!isFavourite) {
+        if(!isFavourite) { // Insert it if isn't.
             this.favourites.push(item);
         }
         console.log(this.favourites);
+        // localStorage.setItem("favourites", JSON.stringify(this.favourites)); // Save favourites.
     }.bind(this);
 
     // this.isFav=function(id){
