2905784622777750451c147550c506f1dc09b5d7
mfmasuyama <mfmasuyama@gmail.com>
2016-11-21 16:52:38 -0300

    Save favourites after refresh.

--- src/app/services/star.js
+++ src/app/services/star.js
@@ -1,37 +1,25 @@
 export function StarService($localStorage) {
 
-    this.click = function(item) { /* FIX */
-        // if(typeof (Storage) !== "undefined") {
-        //     console.log(typeof (Storage));
-        //     if(typeof localStorage.getItem("favourites") !== "undefined" || typeof localStorage.getItem("favourites") === "null") {
-        //         console.log(typeof localStorage.getItem("favourites"));
-        //         let retrievedFavourites = localStorage.getItem("favourites");
-        //         this.favouritesAux = JSON.parse(retrievedFavourites);
-        //         console.log(typeof this.favouritesAux);
-        //         if(typeof this.favouritesAux === "null") {
-        //             this.favourites = new Array();
-        //         } else {
-        //             this.favourites = this.favouritesAux;
-        //             console.log(this.favourites);
-        //         }
-        //     }
-        // }
+    this.favourites = new Array();
 
-        this.favourites = new Array();
-        //Favourites.
+    this.click = function(item) {
+        let retrievedFavourites = localStorage.getItem("favourites");
+        this.favourites = JSON.parse(retrievedFavourites);
         let isFavourite = false;
-        for(let i=0; i<this.favourites.length; i++) { //Check if it's already a favourite.
+        for(let i=0; i<this.favourites.length; i++) {
             if(this.favourites[i].id === item.id) {
                 isFavourite = true;
                 this.favourites.splice(i,1);
             }
         }
-        if(!isFavourite) { // Insert it if isn't.
+        if(!isFavourite) {
             this.favourites.push(item);
         }
+        localStorage.setItem("favourites", JSON.stringify(this.favourites));
         console.log(this.favourites);
-        // localStorage.setItem("favourites", JSON.stringify(this.favourites)); // Save favourites.
     }.bind(this);
+
+
 
     // this.isFav=function(id){
     //     for (var i = $localStorage.favs.length-1; i>=0; i--) {
